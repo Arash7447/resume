5505c1e705cb2031b2ec9b61c3639bca0f663c2e
Arash7447 <arash.sotoudeh6@gmail.com>
2024-03-11 13:22:31 +0330

    the final version of resume
    All parts work without problems

--- website/views.py
+++ website/views.py
@@ -3,12 +3,8 @@ from django.shortcuts import render
 from django.http import HttpResponse, JsonResponse
 
 def index_view(request) :
-    return render(request, "website/index.html")
+    context = {'name':'Arash', 'last_name':'Ghahremani','number':'09129159633','email':'Arash.sotoudeh6@gmail.com'}
+    return render(request, "website/index.html",context)
 
-def fullname_view(request):
-    context = {'name':'Arash', 'last_name':'Ghahremani'}
-    return render (request,'website/index.html',context)
 
-def number_email_view(request) :
-    context = {'number':'09129159633','email':'Arash.sotoudeh6@gmail.com'}
-    return render (request,'index.html',context)
+
